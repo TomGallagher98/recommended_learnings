4e81f739f1fecba08153c5e807b57bf8ffa35e59
TomGallagher98 <79652005+TomGallagher98@users.noreply.github.com>
2021-12-15 15:44:16 +0100

    Add files via upload

---
+++ lang/en/block_recommended_learnings.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Strings for the Recently accessed items block.
+ *
+ * @package    block_recommended_learnings
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'Recommended Activities';
+$string['recommended_learnings'] = 'Recommended Activities';
+$string['blocksettings'] = 'Recommended Activities Settings';
+$string['blockstring'] = 'Select Changes to Block';
+$string['blocktitle'] = 'Configure Text';
+$string['defaulttitle'] = 'Recommended Learning Activities Default';
+$string['defaulttext'] = 'Recommended learning activities default text';
+$string['headerconfig'] = 'Header Configuration';
+$string['descconfig'] = 'Description Configuration';
+$string['recommended_learnings:addinstance'] = 'Add a new Recommended Learning Block';
+$string['recommended_learnings:myaddinstance'] = 'Add a new Recommended Learning Block to the My Moodle Page';
